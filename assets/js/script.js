bc2ef08aa05f23aafec98e057738a4e2ee8013a5
Scott Nichols <s21nichols@hotmail.com>
2021-11-18 19:13:37 -0700

    finalized functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,48 +10,53 @@ var highscoresDiv = document.getElementById("highscores");
 var list = document.getElementById("list");
 var initials = document.getElementById("initials");
 var saveButton = document.getElementById("save");
-var mainDiv = document.getElementById("main-div")
+var restartButton = document.getElementById("restart");
+var mainDiv = document.getElementById("main-div");
 var userScore = 0;
+var timeInterval = null;
 
 var questions = [
   {
   question: "He is Frodo’s loyal friend who journeyed with him from Middle Earth to Mount Doom?",
-  choices: ["Boromir", "Elrond", "Samwise Gamgee", "Bilbo Baggins"],
-  answer: "Samwise Gamgee"
+  choices: ["A. Boromir", "B. Elrond", "C. Samwise Gamgee", "D. Bilbo Baggins"],
+  answer: "C. Samwise Gamgee"
   },
   {
   question: "Which actor played Samwise Gamgee?",
-  choices: ["Sean Astin", "Ashton Kutcher", "Tom Cruise", "Mike Meyers"],
-  answer: "Sean Astin"
+  choices: ["A. Sean Astin", "B. Ashton Kutcher", "C. Tom Cruise", "D. Mike Meyers"],
+  answer: "A. Sean Astin"
   },
   {
   question: "How many rings of power were made for the Elves, Dwarves, and Men?",
-  choices: ["One", "Five", "Five for the Elves, Two for Dwarves, and Eight for Men.", "Three for the Elves, Seven for Dwarves, and Nine for Men."],
-  answer: "Three for the Elves, Seven for Dwarves, and Nine for Men."
+  choices: ["A. One", "B. Five", "C. Five for the Elves, Two for Dwarves, and Eight for Men.", "D. Three for the Elves, Seven for Dwarves, and Nine for Men."],
+  answer: "D. Three for the Elves, Seven for Dwarves, and Nine for Men."
   },
   {
   question: "Who was sent to Rivendell by Denethor to find answers about a dream he had?",
-  choices: ["Theoden", "Boromir", "Pippin", "Gandolf"],
-  answer: "Boromir"
+  choices: ["A. Theoden", "B. Boromir", "C. Pippin", "D. Gandolf"],
+  answer: "B. Boromir"
   },
   {
   question: "What is the name of the mountain where the Master Ring was made?",
-  choices: ["Mount Rainier", "Mount Vesuvius", "Mount Doom", "Mount Olympus"],
-  answer: "Mount Doom"
+  choices: ["A. Mount Rainier", "B. Mount Vesuvius", "C. Mount Doom", "D. Mount Olympus"],
+  answer: "C. Mount Doom"
   }
 ]
 
 function countdown() {
-  var timeInterval = setInterval(function () {
+  timeInterval = setInterval(function () {
     if (timeLeft > 1) {
       timerEl.textContent = timeLeft + ' seconds remaining';
       timeLeft--;
       }  else if (timeLeft === 1) {
         timerEl.textContent = timeLeft + ' second remaining';
         timeLeft--;
-      } else {
-        timerEl.textContent = '';
+      } else if (timeLeft === 0) {
+        alert("Time is Out. Game over!");
+        mainDiv.classList.add("hidden");
+        highscoresDiv.classList.remove("hidden");
         clearInterval(timeInterval);
+        return;
       }
     }, 1000);
   }
@@ -59,29 +64,41 @@ function countdown() {
 
   function startQuiz() {
     countdown();
-    pushButton.setAttribute("class", "hidden");
+    startButton.setAttribute("class", "hidden");
     showQuestion();
+  }
+
+  function restartQuiz() {
+    currentIndex = 0;
+    timeLeft = 60;
+    userScore = 0;
+    mainDiv.classList.remove("hidden");
+    highscoresDiv.classList.add("hidden");
+    startButton.classList.remove("hidden");
+    questionHeading.textContent = "";
+    timerEl.textContent = "";
   }
 
   function showQuestion() {
     answerContainer.innerHTML = "";
-    if (currentIndex === questions.length) {
+    if (timeLeft === 0 || currentIndex > questions.length-1) {
       alert("The quiz is finished!");
       mainDiv.classList.add("hidden");
       highscoresDiv.classList.remove("hidden");
-    return}
+      clearInterval(timeInterval);
+  }
 
     var currentQuestion = questions[currentIndex];
     questionHeading.textContent = currentQuestion.question;
     for (var i = 0; i < currentQuestion.choices.length; i++) {
       var element = currentQuestion.choices[i];
       var button = document.createElement("button");
-      button.textContent = element
+      button.className = ("btn");
+      button.textContent = element;
       button.setAttribute("value", element);
       button.onclick=checkAnswer;
       answerContainer.appendChild(button);
-      console.log(element)
-      
+      console.log(element);
     }
   }
 
@@ -94,10 +111,12 @@ function countdown() {
       // subtract time from clock
     }
     currentIndex++
+
     showQuestion()
   }
 
   startButton.addEventListener("click", startQuiz);
+  restartButton.addEventListener("click", restartQuiz);
 
   var highscores = [];
   if(localStorage.getItem("high scores")) {
@@ -110,7 +129,7 @@ function countdown() {
   }
 
   saveButton.addEventListener("click", function (){
-    var object ={
+    var object = {
       "initials": initials.value,
       "score": userScore
     }
